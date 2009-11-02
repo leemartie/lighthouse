22b541573544341b5dfb0adacb4198ac8581b728
nmoura <nmoura@bcbb7e22-1d7d-42ea-b575-07c612254b40>
2009-11-02 02:19:55 +0000

    create method updateCommittedEvents()

--- LighthouseModel/src/edu/uci/lighthouse/model/jpa/LHEventDAO.java
+++ LighthouseModel/src/edu/uci/lighthouse/model/jpa/LHEventDAO.java
@@ -8,6 +8,13 @@ import java.util.Map;
 import edu.uci.lighthouse.model.LighthouseEvent;
 
 public class LHEventDAO extends AbstractDAO<LighthouseEvent, Integer> {
+	
+	@Override
+	public LighthouseEvent save(LighthouseEvent entity) throws JPAUtilityException {
+		Date timestamp = getCurrentTimestamp();
+		entity.setTimestamp(timestamp);
+		return super.save(entity);
+	}
 
 	public List<LighthouseEvent> executeQueryEntitiesAndTime(
 			Map<String, Date> parameters) {
@@ -17,12 +24,13 @@ public class LHEventDAO extends AbstractDAO<LighthouseEvent, Integer> {
 		for (Map.Entry<String, Date> entry : parameters.entrySet()) {
 			String fqn = entry.getKey();
 			Date timestamp = entry.getValue();
-			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			SimpleDateFormat formatter = new SimpleDateFormat(
+					"yyyy-MM-dd HH:mm:ss");
 			String strTimestamp = formatter.format(timestamp);
 			strQuery += " ( ";
 			strQuery += "event.entity" + " = " + "'" + fqn + "'";
 			strQuery += " AND ";
-			strQuery += "event.timestamp" + " >= " + "'" + strTimestamp + "'" ;
+			strQuery += "event.timestamp" + " >= " + "'" + strTimestamp + "'";
 			strQuery += " ) ";
 			strQuery += "OR ";
 		}
@@ -31,4 +39,21 @@ public class LHEventDAO extends AbstractDAO<LighthouseEvent, Integer> {
 		return executeDynamicQuery(strQuery);
 	}
 	
+	public void updateCommittedEvents(List<String> listClazz, String authorName) {
+		String command = 	"UPDATE LighthouseEvent e " +
+							"SET e.committed = 1 " +
+							"WHERE e.author.name = " + "'" + authorName + "'" + " " +
+							"AND e.entity.fullyQualifiedName IN ( " +
+							"SELECT rel.primaryKey.from " +
+							"FROM LighthouseRelationship rel " +
+							"WHERE rel.primaryKey.type = 0 ";
+		command+= "AND ( ";
+		for (String fqnClazz : listClazz) {
+			command+= "rel.primaryKey.to = " + "'" + fqnClazz + "'";
+			command+= " OR ";
+		}
+		command = command.substring(0, command.lastIndexOf("OR"));
+		command += " ) )";
+		executeUpdateQuery(command);
+	}
 }
