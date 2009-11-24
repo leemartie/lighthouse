3977e3350dffb0ffbfa304a123939aeb24fe4166
nmoura <nmoura@bcbb7e22-1d7d-42ea-b575-07c612254b40>
2009-11-24 09:40:52 +0000

    generate Base LHFile from the database, it is working according with TEST: LighthouseFileManagerTest

--- LighthouseModel/src/edu/uci/lighthouse/model/BuildLHBaseFile.java
+++ LighthouseModel/src/edu/uci/lighthouse/model/BuildLHBaseFile.java
@@ -1,145 +1,80 @@
 package edu.uci.lighthouse.model;
 
-import java.util.Collection;
 import java.util.Date;
 import java.util.LinkedHashSet;
+import java.util.List;
 
-import edu.uci.lighthouse.model.LighthouseEvent.TYPE;
+import edu.uci.lighthouse.model.jpa.LHEventDAO;
 
 public class BuildLHBaseFile {
 
 	public static LighthouseFile execute(LighthouseModel lhModel, String fqnClazz, Date revisionTime, LighthouseAuthor author) {
 		LighthouseFile lhFile = new LighthouseFile();
 		LighthouseFileManager fileManager = new LighthouseFileManager(lhFile);
-		LighthouseEntity clazz = lhModel.getEntity(fqnClazz);
 		LinkedHashSet<LighthouseEntity> listEntitiesInside = new LinkedHashSet<LighthouseEntity>();
-		listEntitiesInside.add(clazz);
-		Collection<LighthouseRelationship> listRel = lhModel.getRelationshipsTo(clazz);
-		for (LighthouseRelationship rel : listRel) {
-			// get all entities inside the fqnClazz
-			if (rel.getType()==LighthouseRelationship.TYPE.INSIDE) {
-				LighthouseEntity entityInside = rel.getFromEntity();
-				listEntitiesInside.add(entityInside);
-				if (isInnerClass(entityInside)) {
-					// I need to create just one method with recurssion or use a stack
-					Collection<LighthouseRelationship> listInnerRel = lhModel.getRelationshipsTo(entityInside);
-					for (LighthouseRelationship relInner : listInnerRel) {
-						// get all entities inside the innerClazz
-						if (relInner.getType()==LighthouseRelationship.TYPE.INSIDE) {
-							LighthouseEntity innerEntityInside = relInner.getFromEntity();
-							listEntitiesInside.add(innerEntityInside);
+		listEntitiesInside = selectEntitiesInside(listEntitiesInside,lhModel,fqnClazz);
+
+		List<LighthouseEvent> listEvents = new LHEventDAO().executeQueryLhBaseFile(listEntitiesInside,revisionTime,author);
+
+		for (LighthouseEvent event : listEvents) {
+			System.out.println(event);
+			if (event.getType()==LighthouseEvent.TYPE.ADD) {
+				if (!wasEventRemoved(listEvents,event)) {
+					Object artifact = event.getArtifact();
+					if (artifact instanceof LighthouseRelationship) {
+						if (!isValidRelationship(artifact, listEntitiesInside)) {
+							continue; // do NOT add relationship
 						}
 					}
+					fileManager.addArtifact(event.getArtifact());
 				}
 			}
 		}
-		// for each entity inside
-		for (LighthouseEntity entity : listEntitiesInside) {
-			Collection<LighthouseEvent> listEntityEvents = lhModel.getEvents(entity);
-			boolean shouldAddEntity = false;
-			for (LighthouseEvent event : listEntityEvents) {
-				if (event.isCommitted()) {
-					if (event.getType() == TYPE.ADD) {
-						shouldAddEntity = true;
-					} else if (event.getType() == TYPE.REMOVE) {
-						shouldAddEntity = false;
-						break;
-					}
-				} 
-//				else { // if is NOT committed
-//					// check if the author create a new entity in his own working copy after the revision time
-//					if (event.getAuthor().equals(author)) {
-//						if (revisionTime.before(event.getTimestamp())) {
-//							if (event.getType() == TYPE.ADD) {
-//								shouldAddEntity = true;
-//							} else if (event.getType() == TYPE.REMOVE) {
-//								shouldAddEntity = false;
-//								break;
-//							}
-//						}
-//					}
-//				}
-			} // end-for-events
-			if (shouldAddEntity) {
-				fileManager.addEntity(entity);
-				Collection<LighthouseRelationship> listRelFrom = lhModel.getRelationshipsFrom(entity);
-				Collection<LighthouseRelationship> listRelTo = lhModel.getRelationshipsTo(entity);
-				
-				for (LighthouseRelationship relationship : listRelFrom) {
-					Collection<LighthouseEvent> listRelEvents = lhModel.getEvents(relationship);
-					boolean shouldAddRel = false;
-					for (LighthouseEvent event : listRelEvents) {
-						if (event.isCommitted()) {
-							if (event.getType() == TYPE.ADD) {
-								shouldAddRel = true;
-							} else if (event.getType() == TYPE.REMOVE) {
-								shouldAddRel = false;
-								break;
-							}
-						} 
-//						else { // if is NOT committed
-//							// check if the author create a new entity in his own working copy after the revision time
-//							if (event.getAuthor().equals(author)) {
-//								if (revisionTime.before(event.getTimestamp())) {
-//									if (event.getType() == TYPE.ADD) {
-//										shouldAddEntity = true;
-//									} else if (event.getType() == TYPE.REMOVE) {
-//										shouldAddEntity = false;
-//										break;
-//									}
-//								}
-//							}
-//						}
-					}
-					if (shouldAddRel) {
-						fileManager.addRelationship(relationship);
-					}
-				}
-				for (LighthouseRelationship relationship : listRelTo) {
-					boolean shouldAddRel = false;
-					if (!listEntitiesInside.contains(relationship.getFromEntity())) {
-						if (	relationship.getType()==LighthouseRelationship.TYPE.CALL 
-								|| relationship.getType()==LighthouseRelationship.TYPE.USES
-								|| relationship.getType()==LighthouseRelationship.TYPE.HOLDS) {
-							shouldAddRel = false;
-							break;
-						}
-					}
-					Collection<LighthouseEvent> listRelEvents = lhModel.getEvents(relationship);
-					for (LighthouseEvent event : listRelEvents) {
-						if (event.isCommitted()) {
-							if (event.getType() == TYPE.ADD) {
-								shouldAddRel = true;
-							} else if (event.getType() == TYPE.REMOVE) {
-								shouldAddRel = false;
-								break;
-							}
-						} 
-//						else { // if is NOT committed
-//							// check if the author create a new entity in his own working copy after the revision time
-//							if (event.getAuthor().equals(author)) {
-//								if (revisionTime.before(event.getTimestamp())) {
-//									if (event.getType() == TYPE.ADD) {
-//										shouldAddEntity = true;
-//									} else if (event.getType() == TYPE.REMOVE) {
-//										shouldAddEntity = false;
-//										break;
-//									}
-//								}
-//							}
-//						}
-					}
-					if (shouldAddRel) {
-						fileManager.addRelationship(relationship);
-					}
-				}
-			} // end-if-shouldAdd
-		}
 		return lhFile;
 	}
+
+	private static boolean isValidRelationship(Object artifact, LinkedHashSet<LighthouseEntity> listEntitiesInside) {
+		boolean result = true;
+		LighthouseRelationship rel = (LighthouseRelationship) artifact;
+		if (!listEntitiesInside.contains(rel.getFromEntity())) {
+			if (	rel.getType()==LighthouseRelationship.TYPE.CALL 
+					|| rel.getType()==LighthouseRelationship.TYPE.USES
+					|| rel.getType()==LighthouseRelationship.TYPE.HOLDS) {
+				result = false;
+			}
+		}
+		return result;
+	}
 	
-	private static boolean isInnerClass(LighthouseEntity entity) {
-		return (entity.getFullyQualifiedName().indexOf("$")!=-1);
+	private static boolean wasEventRemoved(List<LighthouseEvent> listEvents, LighthouseEvent paramEvent) {
+		for (LighthouseEvent event : listEvents) {
+			if (event.getArtifact().equals(paramEvent.getArtifact())) {
+				if (event.isCommitted()==true) {
+					if (event.getType() == LighthouseEvent.TYPE.REMOVE) {
+						return true;
+					}					
+				}
+			}
+		}
+		return false;
+	}
+
+	/**Recursion
+	 * 
+	 * @param listEntitiesInside should be a new LinkedHashSet()
+	 * 
+	 * */ 
+	private static LinkedHashSet<LighthouseEntity> selectEntitiesInside(LinkedHashSet<LighthouseEntity> listEntitiesInside, LighthouseModel lhModel, String fqnClazz) {
+		LighthouseModelManager lhModelManager = new LighthouseModelManager(lhModel);
+		List<LighthouseEntity> subListEntitiesInside = lhModelManager.getEntitiesInsideClass(fqnClazz);
+		for (LighthouseEntity entity : subListEntitiesInside) {
+			if (entity instanceof LighthouseClass || entity instanceof LighthouseInterface) { // That is a Inner class
+				listEntitiesInside.addAll(selectEntitiesInside(listEntitiesInside, lhModel, entity.getFullyQualifiedName()));
+			}
+		}
+		listEntitiesInside.addAll(subListEntitiesInside);
+		listEntitiesInside.add(lhModelManager.getEntity(fqnClazz));
+		return listEntitiesInside;
 	}
 	
 }
