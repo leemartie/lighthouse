3bb0292788dce6cb7c7a25eaaf6e0a7012983353
nmoura <nmoura@bcbb7e22-1d7d-42ea-b575-07c612254b40>
2009-11-26 01:22:25 +0000

    move methods to LighthouseModelUtil

--- LighthouseModel/src/edu/uci/lighthouse/model/BuildLHBaseFile.java
+++ LighthouseModel/src/edu/uci/lighthouse/model/BuildLHBaseFile.java
@@ -11,71 +11,24 @@ public class BuildLHBaseFile {
 	public static LighthouseFile execute(LighthouseModel lhModel, String fqnClazz, Date revisionTime, LighthouseAuthor author) {
 		LighthouseFile lhFile = new LighthouseFile();
 		LighthouseFileManager fileManager = new LighthouseFileManager(lhFile);
-		LinkedHashSet<LighthouseEntity> listEntitiesInside = new LinkedHashSet<LighthouseEntity>();
-		listEntitiesInside = selectEntitiesInside(listEntitiesInside,lhModel,fqnClazz);
-
+		LinkedHashSet<LighthouseEntity> listEntitiesInside = new LighthouseModelManager(lhModel).selectEntitiesInsideClass(fqnClazz);
 		List<LighthouseEvent> listEvents = new LHEventDAO().executeQueryLhBaseFile(listEntitiesInside,revisionTime,author);
-
-		for (LighthouseEvent event : listEvents) {
-			if (event.getType()==LighthouseEvent.TYPE.ADD) {
-				if (!wasEventRemoved(listEvents,event)) {
-					Object artifact = event.getArtifact();
-					if (artifact instanceof LighthouseRelationship) {
-						if (!isValidRelationship(artifact, listEntitiesInside)) {
-							continue; // do NOT add relationship
+		if (listEvents!=null && listEvents.size()!=0) {
+			for (LighthouseEvent event : listEvents) {
+				if (event.getType()==LighthouseEvent.TYPE.ADD) {
+					if (!LighthouseModelUtil.wasEventRemoved(listEvents,event)) {
+						Object artifact = event.getArtifact();
+						if (artifact instanceof LighthouseRelationship) {
+							if (!LighthouseModelUtil.isValidRelationship(artifact, listEntitiesInside)) {
+								continue; // do NOT add relationship
+							}
 						}
+						fileManager.addArtifact(event.getArtifact());
 					}
-					fileManager.addArtifact(event.getArtifact());
 				}
 			}
 		}
 		return lhFile;
 	}
 
-	private static boolean isValidRelationship(Object artifact, LinkedHashSet<LighthouseEntity> listEntitiesInside) {
-		boolean result = true;
-		LighthouseRelationship rel = (LighthouseRelationship) artifact;
-		if (!listEntitiesInside.contains(rel.getFromEntity())) {
-			if (	rel.getType()==LighthouseRelationship.TYPE.CALL 
-					|| rel.getType()==LighthouseRelationship.TYPE.USES
-					|| rel.getType()==LighthouseRelationship.TYPE.HOLDS
-					|| rel.getType()==LighthouseRelationship.TYPE.RECEIVES
-					|| rel.getType()==LighthouseRelationship.TYPE.RETURN) {
-				result = false;
-			}
-		}
-		return result;
-	}
-	
-	private static boolean wasEventRemoved(List<LighthouseEvent> listEvents, LighthouseEvent paramEvent) {
-		for (LighthouseEvent event : listEvents) {
-			if (event.getArtifact().equals(paramEvent.getArtifact())) {
-				if (event.isCommitted()==true) {
-					if (event.getType() == LighthouseEvent.TYPE.REMOVE) {
-						return true;
-					}					
-				}
-			}
-		}
-		return false;
-	}
-
-	/**Recursion
-	 * 
-	 * @param listEntitiesInside should be a new LinkedHashSet()
-	 * 
-	 * */ 
-	private static LinkedHashSet<LighthouseEntity> selectEntitiesInside(LinkedHashSet<LighthouseEntity> listEntitiesInside, LighthouseModel lhModel, String fqnClazz) {
-		LighthouseModelManager lhModelManager = new LighthouseModelManager(lhModel);
-		List<LighthouseEntity> subListEntitiesInside = lhModelManager.getEntitiesInsideClass(fqnClazz);
-		for (LighthouseEntity entity : subListEntitiesInside) {
-			if (entity instanceof LighthouseClass || entity instanceof LighthouseInterface) { // That is a Inner class
-				listEntitiesInside.addAll(selectEntitiesInside(listEntitiesInside, lhModel, entity.getFullyQualifiedName()));
-			}
-		}
-		listEntitiesInside.addAll(subListEntitiesInside);
-		listEntitiesInside.add(lhModelManager.getEntity(fqnClazz));
-		return listEntitiesInside;
-	}
-	
 }
