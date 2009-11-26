7fe2fbc8681931f3de4d6ad8f982444359e3c253
nmoura <nmoura@bcbb7e22-1d7d-42ea-b575-07c612254b40>
2009-11-26 01:25:53 +0000

    Finish the first version of executeQueryCheckOut()

--- LighthouseModel/src/edu/uci/lighthouse/model/jpa/LHEventDAO.java
+++ LighthouseModel/src/edu/uci/lighthouse/model/jpa/LHEventDAO.java
@@ -16,69 +16,68 @@ public class LHEventDAO extends AbstractDAO<LighthouseEvent, Integer> {
 	public List<LighthouseEvent> executeQueryCheckOut(
 			Map<String, Date> parameters) {
 		List<LighthouseEvent> result = null;
-//		if (parameters.size()>0) {			
-//			String strQuery = "SELECT e " + "FROM LighthouseEvent e "
-//			+ "WHERE ";
-//			strQuery += " ( ";
-//			
-//			// Get all entities' events
-//			for (Map.Entry<String, Date> entry : parameters.entrySet()) {
-//				String fqn = entry.getKey();
-//				Date timestamp = entry.getValue();
-//				SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//				String strTimestamp = formatter.format(timestamp);
-//				strQuery += " ( ( ";
-//				strQuery += "e.entity" + " = " + "'" + fqn + "'";
-//				strQuery += " AND ";
-//				strQuery += "e.timestamp" + " >= " + "'" + strTimestamp + "'";
-//				strQuery += " ) OR ( ( ";
-//				strQuery += "e.entity" + " = " + "'" + fqn + "'";
-//				strQuery += " AND ";
-//				strQuery += "e.isCommitted" + " = " + "1";
-//				strQuery += " ) AND ";
-//				strQuery += " NOT ( ";
-//				strQuery += "e.type" + " = " + "1"; // type==remove
-//				strQuery += " AND ";
-//				strQuery += "e.committedTime" + " <= " + "'" + strTimestamp + "'";
-//				strQuery += " ) ) ) ";
-//				strQuery += "OR ";
-//			}
-//			
-//			// Get all relationships' events
-//			for (Map.Entry<String, Date> entry : parameters.entrySet()) {
-//				String fqn = entry.getKey();
-//				Date timestamp = entry.getValue();
-//				SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//				String strTimestamp = formatter.format(timestamp);
-//				strQuery += " ( ( ";
-//				strQuery += " ( ";
-//				strQuery += "e.relationship.primaryKey.from" + " = " + "'" + fqn + "'";
-//				strQuery += " OR ";
-//				strQuery += "e.relationship.primaryKey.to" + " = " + "'" + fqn + "'";
-//				strQuery += " ) ";
-//				strQuery += " AND ";
-//				strQuery += "e.timestamp" + " >= " + "'" + strTimestamp + "'";
-//				strQuery += " ) OR ( ( ";
-//				strQuery += " ( ";
-//				strQuery += "e.relationship.primaryKey.from" + " = " + "'" + fqn + "'";
-//				strQuery += " OR ";
-//				strQuery += "e.relationship.primaryKey.to" + " = " + "'" + fqn + "'";
-//				strQuery += " ) ";
-//				strQuery += " AND ";
-//				strQuery += "e.isCommitted" + " = " + "1";
-//				strQuery += " ) AND ";
-//				strQuery += " NOT ( ";
-//				strQuery += "e.type" + " = " + "1"; // type==remove
-//				strQuery += " AND ";
-//				strQuery += "e.committedTime" + " <= " + "'" + strTimestamp + "'";
-//				strQuery += " ) ) ) ";
-//				strQuery += "OR ";
-//			}
-//			
-//			strQuery = strQuery.substring(0, strQuery.lastIndexOf("OR"));
-//			strQuery += " )";
-//			result = executeDynamicQuery(strQuery);
-//		}
+		if (parameters.size()>0) {			
+			String strQuery = "SELECT e " + "FROM LighthouseEvent e "
+			+ "WHERE ";
+			strQuery += " ( ";
+			// Get all entities' events
+			for (Map.Entry<String, Date> entry : parameters.entrySet()) {
+				String fqn = entry.getKey();
+				Date timestamp = entry.getValue();
+				SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				String strTimestamp = formatter.format(timestamp);
+				strQuery += " ( ( ";
+				strQuery += "e.entity" + " = " + "'" + fqn + "'";
+				strQuery += " AND ";
+				strQuery += "e.timestamp" + " >= " + "'" + strTimestamp + "'";
+				strQuery += " ) OR ( ( ";
+				strQuery += "e.entity" + " = " + "'" + fqn + "'";
+				strQuery += " AND ";
+				strQuery += "e.isCommitted" + " = " + true;
+				strQuery += " AND ";
+				strQuery += "e.committedTime" + " <= " + "'" + strTimestamp + "'";
+				strQuery += " ) AND ( ";
+				strQuery += "e.type" + " = " + TYPE.ADD.ordinal();
+				strQuery += " OR ";
+				strQuery += "e.type" + " = " + TYPE.REMOVE.ordinal();
+				strQuery += " ) ) ) ";
+				strQuery += "OR ";
+			}
+			// Get all relationships' events
+			for (Map.Entry<String, Date> entry : parameters.entrySet()) {
+				String fqn = entry.getKey();
+				Date timestamp = entry.getValue();
+				SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				String strTimestamp = formatter.format(timestamp);
+				strQuery += " ( ( ";
+				strQuery += " ( ";
+				strQuery += "e.relationship.primaryKey.from" + " = " + "'" + fqn + "'";
+				strQuery += " OR ";
+				strQuery += "e.relationship.primaryKey.to" + " = " + "'" + fqn + "'";
+				strQuery += " ) ";
+				strQuery += " AND ";
+				strQuery += "e.timestamp" + " >= " + "'" + strTimestamp + "'";
+				strQuery += " ) OR ( ( ";
+				strQuery += " ( ";
+				strQuery += "e.relationship.primaryKey.from" + " = " + "'" + fqn + "'";
+				strQuery += " OR ";
+				strQuery += "e.relationship.primaryKey.to" + " = " + "'" + fqn + "'";
+				strQuery += " ) ";
+				strQuery += " AND ";
+				strQuery += "e.isCommitted" + " = " + true;
+				strQuery += " AND ";
+				strQuery += "e.committedTime" + " <= " + "'" + strTimestamp + "'";
+				strQuery += " ) AND ( ";
+				strQuery += "e.type" + " = " + TYPE.ADD.ordinal();
+				strQuery += " OR ";
+				strQuery += "e.type" + " = " + TYPE.REMOVE.ordinal();
+				strQuery += " ) ) ) ";
+				strQuery += "OR ";
+			}
+			strQuery = strQuery.substring(0, strQuery.lastIndexOf("OR"));
+			strQuery += " )";
+			result = executeDynamicQuery(strQuery);
+		}
 		return result;
 	}
 	
