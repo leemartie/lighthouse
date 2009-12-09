3e236ff8e4f1d3825a9977a142e1731fcab3f55f
nmoura <nmoura@bcbb7e22-1d7d-42ea-b575-07c612254b40>
2009-12-09 10:08:45 +0000

    PERFECT VERSION - Nilmax clean every little detail, even ctrl+f

--- LighthouseProjectSamples/AtmDeitelSample/src/edu/prenticehall/deitel/Account.java
+++ LighthouseProjectSamples/AtmDeitelSample/src/edu/prenticehall/deitel/Account.java
@@ -3,79 +3,65 @@ package edu.prenticehall.deitel;
 // Account.java
 // Represents a bank account
 
-public class Account 
-{
-   private int accountNumber; // account number
-   private int pin; // PIN for authentication
-   private double availableBalance; // funds available for withdrawal
-   private double totalBalance; // funds available + pending deposits
-   
-   public int b;
-   public int c;
-   
-   // Account constructor initializes attributes
-   public Account( int theAccountNumber, int thePIN, 
-      double theAvailableBalance, double theTotalBalance )
-   {
-      accountNumber = theAccountNumber;
-      pin = thePIN;
-      availableBalance = theAvailableBalance;
-      totalBalance = theTotalBalance;
-   } // end Account constructor
+public class Account {
+	private int accountNumber; // account number
+	private int pin; // PIN for authentication
+	private double availableBalance; // funds available for withdrawal
+	private double totalBalance; // funds available + pending deposits
 
-   // determines whether a user-specified PIN matches PIN in Account
-   public boolean validatePIN( int userPIN )
-   {
-      if ( userPIN == pin )
-         return true;
-      else
-         return false;
-   } // end method validatePIN
-   
-   // returns available balance
-   public double getAvailableBalance()
-   {
-      return availableBalance;
-   } // end getAvailableBalance
+	// Account constructor initializes attributes
+	public Account(int theAccountNumber, int thePIN,
+			double theAvailableBalance, double theTotalBalance) {
+		accountNumber = theAccountNumber;
+		pin = thePIN;
+		availableBalance = theAvailableBalance;
+		totalBalance = theTotalBalance;
+	} // end Account constructor
 
-   // returns the total balance
-   public double getTotalBalance()
-   {
-      return totalBalance;
-   } // end method getTotalBalance
+	// determines whether a user-specified PIN matches PIN in Account
+	public boolean validatePIN(int userPIN) {
+		if (userPIN == pin)
+			return true;
+		else
+			return false;
+	} // end method validatePIN
 
-   // credits an amount to the account
-   public void credit( double amount )
-   {
-      totalBalance += amount; // add to total balance
-   } // end method credit
+	// returns available balance
+	public double getAvailableBalance() {
+		return availableBalance;
+	} // end getAvailableBalance
 
-   // debits an amount from the account
-   public void debit( double amount )
-   {
-      availableBalance -= amount; // subtract from available balance
-      totalBalance -= amount; // subtract from total balance
-   } // end method debit
+	// returns the total balance
+	public double getTotalBalance() {
+		return totalBalance;
+	} // end method getTotalBalance
 
-   // returns account number
-   public int getAccountNumber()
-   {
-      return accountNumber;  
-   } // end method getAccountNumber
+	// credits an amount to the account
+	public void credit(double amount) {
+		totalBalance += amount; // add to total balance
+	} // end method credit
+
+	// debits an amount from the account
+	public void debit(double amount) {
+		availableBalance -= amount; // subtract from available balance
+		totalBalance -= amount; // subtract from total balance
+	} // end method debit
+
+	// returns account number
+	public int getAccountNumber() {
+		return accountNumber;
+	} // end method getAccountNumber
 } // end class Account
 
-
 /**************************************************************************
- * (C) Copyright 1992-2005 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
+ * (C) Copyright 1992-2005 by Deitel & Associates, Inc. and * Pearson Education,
+ * Inc. All Rights Reserved. * * DISCLAIMER: The authors and publisher of this
+ * book have used their * best efforts in preparing the book. These efforts
+ * include the * development, research, and testing of the theories and programs
+ * * to determine their effectiveness. The authors and publisher make * no
+ * warranty of any kind, expressed or implied, with regard to these * programs
+ * or to the documentation contained in these books. The authors * and publisher
+ * shall not be liable in any event for incidental or * consequential damages in
+ * connection with, or arising out of, the * furnishing, performance, or use of
+ * these programs. *
  *************************************************************************/
