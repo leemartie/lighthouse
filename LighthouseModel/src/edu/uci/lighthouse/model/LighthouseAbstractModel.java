cb8b02bf3f96beddc4e3bd181b49bfebc9807e71
nmoura <nmoura@bcbb7e22-1d7d-42ea-b575-07c612254b40>
2010-01-12 19:34:36 +0000

    add some comments

--- LighthouseModel/src/edu/uci/lighthouse/model/LighthouseAbstractModel.java
+++ LighthouseModel/src/edu/uci/lighthouse/model/LighthouseAbstractModel.java
@@ -9,26 +9,31 @@ import java.util.List;
 import edu.uci.lighthouse.model.LighthouseRelationship.TYPE;
 
 /**
- * @author nilmax
- */
+ * It consist of a list of entities and a list of relationships
+ * */
 public abstract class LighthouseAbstractModel {
 
-	/** */
 	private HashMap<String, LighthouseEntity> entities = new HashMap<String, LighthouseEntity>();
 
-	/** */
 	private HashMap<String, LinkedHashSet<LighthouseRelationship>> relationships = new HashMap<String, LinkedHashSet<LighthouseRelationship>>();
 
-	// only ModelManager is allowed to call this method
+	/** 
+	 * Only {@link LighthouseModelManager} is allowed to call this method
+	 * */
 	final synchronized void addEntity(LighthouseEntity entity) {
 		entities.put(entity.getFullyQualifiedName(), entity);
 	}
 
-	final void removeEntity(LighthouseEntity entity) {
+	/** 
+	 * Only {@link LighthouseModelManager} is allowed to call this method
+	 * */
+	final synchronized void removeEntity(LighthouseEntity entity) {
 		entities.remove(entity.getFullyQualifiedName());
 	}
 	
-	// only ModelManager is allowed to call this method
+	/** 
+	 * Only {@link LighthouseModelManager} is allowed to call this method
+	 * */
 	final synchronized void addRelationship(LighthouseRelationship rel) {
 		LighthouseEntity e = rel.getToEntity();		
 		LinkedHashSet<LighthouseRelationship> list = relationships.get(e
@@ -40,7 +45,10 @@ public abstract class LighthouseAbstractModel {
 		list.add(rel);
 	}
 	
-	final void removeRelationship(LighthouseRelationship rel) {
+	/** 
+	 * Only {@link LighthouseModelManager} is allowed to call this method
+	 * */
+	final synchronized void removeRelationship(LighthouseRelationship rel) {
 		LighthouseEntity entity = rel.getToEntity();
 		Collection<LighthouseRelationship> list = relationships.get(entity.getFullyQualifiedName());
 		if (list!=null) { 
@@ -48,16 +56,17 @@ public abstract class LighthouseAbstractModel {
 		}
 	}
 
+	/**
+	 * Verify if a entity exist in the model
+	 * */
 	public boolean containsEntity(String fqn) {
 		return (this.getEntity(fqn)!=null);
 	}
 	
-	/** */
 	public LighthouseEntity getEntity(String fqn) {
 		return entities.get(fqn);
 	}
 	
-	/** */
 	public Collection<LighthouseEntity> getMethodsAndAttributesFromClass(
 			LighthouseClass c) {
 		LinkedList<LighthouseEntity> result = new LinkedList<LighthouseEntity>();
@@ -75,7 +84,6 @@ public abstract class LighthouseAbstractModel {
 		return result;
 	}
 	
-	/** */
 	public Collection<LighthouseClass> getAllClasses() {
 		LinkedList<LighthouseClass> list = new LinkedList<LighthouseClass>();
 		for (LighthouseEntity e : entities.values()) {
@@ -86,7 +94,6 @@ public abstract class LighthouseAbstractModel {
 		return list;
 	}
 	
-	/** */
 	public Collection<LighthouseEntity> getEntities() {
 		return entities.values();
 	}
@@ -97,7 +104,7 @@ public abstract class LighthouseAbstractModel {
 	}
 	
 	/**
-	 * Verify the <code>relationship</code> exist in the model
+	 * Verify if the <code>relationship</code> exist in the model
 	 * @return the relationship instance from the model 
 	 * */
 	public LighthouseRelationship getRelationship(LighthouseRelationship relationship) {
@@ -110,7 +117,6 @@ public abstract class LighthouseAbstractModel {
 		return null;
 	}
 	
-	/** */
 	public LinkedHashSet<LighthouseRelationship> getRelationships() {
 		LinkedHashSet<LighthouseRelationship> result = new LinkedHashSet<LighthouseRelationship>();
 		for (LinkedHashSet<LighthouseRelationship> list : relationships.values()) {			
@@ -119,14 +125,6 @@ public abstract class LighthouseAbstractModel {
 		return result;
 	}
 	
-//	public Collection<LighthouseRelationship> getRelationships(LighthouseEntity entity) {
-//		List<LighthouseRelationship> result = new LinkedList<LighthouseRelationship>();
-//		result.addAll(getRelationshipsFrom(entity));
-//		result.addAll(getRelationshipsTo(entity));
-//		return result;
-//	}
-	
-	/** */
 	public Collection<LighthouseRelationship> getRelationshipsFrom(LighthouseEntity entity) {
 		List<LighthouseRelationship> result = new LinkedList<LighthouseRelationship>();
 		// FIXME Use this code for optimization - relationships.get(entity.getFullyQualifiedName()).iterator();
@@ -151,11 +149,6 @@ public abstract class LighthouseAbstractModel {
 		return result;
 	}
 
-	public Collection<LighthouseRelationship> getRelationships(LighthouseEntity entity) {
-		// FIXME just return the TO relationship :(
-		return relationships.get(entity.getFullyQualifiedName());
-	}
-	
 	public Collection<LighthouseRelationship> getRelationshipsTo(LighthouseEntity entity, TYPE type) {
 		List<LighthouseRelationship> result = new LinkedList<LighthouseRelationship>();
 		// FIXME Use this code for optimization - relationships.get(entity.getFullyQualifiedName()).iterator();
@@ -169,69 +162,5 @@ public abstract class LighthouseAbstractModel {
 		}
 		return result;
 	}
-	
-//	public Collection<LighthouseRelationship> getRelationshipsTo(LighthouseEntity e) {
-//		List<LighthouseRelationship> result = new LinkedList<LighthouseRelationship>();
-//		Collection<LighthouseRelationship> list = relationships.get(e.getFullQualifiedName());
-//		if (list != null)
-//			result.addAll(list);
-//		return result;
-//	}
-//	
-//	public Collection<LighthouseEntity> getSubclasses(LighthouseEntity e) {
-//		Collection<LighthouseEntity> result = new LinkedList<LighthouseEntity>();
-//		Collection<LighthouseRelationship> list = this.getRelationshipsFrom(e);
-//		for (LighthouseRelationship r : list)
-//		{
-//			if ( (r.getType() == TYPE.EXTENDS)
-//					|| (r.getType() == TYPE.IMPLEMENTS) )
-//			{
-//				result.add(r.getToEntity());
-//			}
-//		}
-//		return result;
-//	}
-//
-//	public Collection<LighthouseEntity> getAllDescendants(LighthouseEntity e) {
-//
-//		Collection<LighthouseRelationship> list = this.getRelationshipsTo(e);
-//		
-//		Collection<LighthouseEntity> result = new LinkedList<LighthouseEntity>();
-//		for (LighthouseRelationship r : list)
-//		{
-//			if ( (r.getType() == TYPE.EXTENDS)
-//					|| (r.getType() == TYPE.IMPLEMENTS) )
-//			{
-//				result.add(r.getFromEntity());
-//			}
-//		}
-//		
-//		if (result.size() > 0)
-//		{
-//			Collection<LighthouseEntity> nextLevelDescendants = new LinkedList<LighthouseEntity>();
-//			for (LighthouseEntity e2: result)
-//			{
-//				nextLevelDescendants.addAll(getAllDescendants(e2)); 
-//			}
-//			result.addAll(nextLevelDescendants);
-//		}
-//		
-//		return result;
-//	}
-//
-//	public LighthouseEntity getContainingClass (LighthouseEntity e)
-//	{
-//		Collection<LighthouseRelationship> list = this.getRelationshipsFrom(e);
-//		
-//		for (LighthouseRelationship r : list)
-//		{
-//			if  (r.getType() == TYPE.INSIDE)
-//			{
-//				return r.getToEntity();
-//			}
-//		}
-//
-//		return e;
-//	}
 	
 }
