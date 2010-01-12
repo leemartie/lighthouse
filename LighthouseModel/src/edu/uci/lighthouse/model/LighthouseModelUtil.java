b92767a0df5fa24880ecd2aee7ae6e210397870b
nmoura <nmoura@bcbb7e22-1d7d-42ea-b575-07c612254b40>
2010-01-12 22:33:03 +0000

    add some comments

--- LighthouseModel/src/edu/uci/lighthouse/model/LighthouseModelUtil.java
+++ LighthouseModel/src/edu/uci/lighthouse/model/LighthouseModelUtil.java
@@ -8,38 +8,27 @@ import edu.uci.lighthouse.model.LighthouseRelationship.TYPE;
 
 public class LighthouseModelUtil {
 
-	// used by BuildLHBaseFile and PullModel
-	public static boolean isValidRelationship(Object artifact, Collection<LighthouseEntity> listEntitiesInside) {
+	/**
+	 * Verify if the <code>relationship</code> "BELONGS" to a class(listEntitiesInside)
+	 * */
+	public static boolean isValidRelationship(LighthouseRelationship relationship, Collection<LighthouseEntity> listEntitiesInside) {
 		boolean result = true; 
-		LighthouseRelationship rel = (LighthouseRelationship) artifact;
-		if (!listEntitiesInside.contains(rel.getFromEntity())) {
-			if (	rel.getType()==LighthouseRelationship.TYPE.CALL 
-					|| rel.getType()==LighthouseRelationship.TYPE.USES
-					|| rel.getType()==LighthouseRelationship.TYPE.HOLDS
-					|| rel.getType()==LighthouseRelationship.TYPE.RECEIVES
-					|| rel.getType()==LighthouseRelationship.TYPE.RETURN) {
+		if (!listEntitiesInside.contains(relationship.getFromEntity())) {
+			if (	relationship.getType()==LighthouseRelationship.TYPE.CALL 
+					|| relationship.getType()==LighthouseRelationship.TYPE.USES
+					|| relationship.getType()==LighthouseRelationship.TYPE.HOLDS
+					|| relationship.getType()==LighthouseRelationship.TYPE.RECEIVES
+					|| relationship.getType()==LighthouseRelationship.TYPE.RETURN) {
 				result = false;
 			}
 		}
 		return result;
 	}
-	
-	// used by checkout
-	public static boolean wasCommittedEventRemoved(List<LighthouseEvent> listEvents, LighthouseEvent paramEvent) {
-		for (LighthouseEvent event : listEvents) {
-			if (event.getArtifact().equals(paramEvent.getArtifact())) {
-				if (	event.isCommitted() &&
-						event.getType() == LighthouseEvent.TYPE.REMOVE && 
-						(	event.getCommittedTime().after(paramEvent.getCommittedTime()) ||
-							event.getCommittedTime().equals(paramEvent.getCommittedTime()) ) ) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
-	// used by BuildBaseFile
+
+	/**
+	 * Verify if an <code>paramEvent</code> was removed by some user.
+	 * It means that there is an NEWER event that has TYPE==REMOVE and it was already COMMITTED
+	 * */
 	public static boolean wasEventRemoved(List<LighthouseEvent> listEvents, LighthouseEvent paramEvent, LighthouseAuthor paramAuthor) {
 		for (LighthouseEvent event : listEvents) {
 			if (event.getArtifact().equals(paramEvent.getArtifact())) {
@@ -62,16 +51,46 @@ public class LighthouseModelUtil {
 		return false;
 	}
 	
+	/**
+	 * Get Entities INSIDE a class
+	 * */
+	private static LinkedHashSet<LighthouseEntity> getEntitiesInsideClass(LighthouseModel model, LighthouseEntity clazz) {
+		LinkedHashSet<LighthouseEntity> listEntity = new LinkedHashSet<LighthouseEntity>();
+		Collection<LighthouseRelationship> listRelInside = model.getRelationshipsTo(clazz,TYPE.INSIDE);
+		for (LighthouseRelationship rel : listRelInside) {
+			LighthouseEntity entity = rel.getFromEntity();
+			listEntity.add(entity);
+		}
+		return listEntity;
+	}
+	
+	/**
+	 * for each class - Get Entities INSIDE a class
+	 * */
+	public static Collection<LighthouseEntity> getEntitiesInsideClasses(LighthouseModel model, Collection<String> listClazzFqn) {
+		LinkedHashSet<LighthouseEntity> listEntity = new LinkedHashSet<LighthouseEntity>();
+		for (String clazzFqn : listClazzFqn) {
+			LighthouseEntity clazz = model.getEntity(clazzFqn);
+			listEntity.add(clazz);
+			listEntity.addAll(getEntitiesInsideClass(model, clazz));
+		}
+		return listEntity;
+	}
+	
+	/**
+	 * for each class - Get events related to entities/relationship that are INSIDE a class
+	 * */
 	public static Collection<LighthouseEvent> getEventsInside(LighthouseModel model, Collection<String> listClazzFqn) {
 		Collection<LighthouseEntity> listEntity = getEntitiesInsideClasses(model, listClazzFqn);
 		Collection<LighthouseRelationship> listRel = getRelationships(model, listEntity);
-		LinkedHashSet<LighthouseEvent> listEvents = getEventsInside(model, listEntity, listRel);
+		LinkedHashSet<LighthouseEvent> listEvents = getEventsByListEntityAndRel(model, listEntity, listRel);
 		return listEvents;
 	}
-
-	public static LinkedHashSet<LighthouseEvent> getEventsInside(
-			LighthouseModel model, Collection<LighthouseEntity> listEntity,
-			Collection<LighthouseRelationship> listRel) {
+	
+	/**
+	 * Get Events related to a list of Entities and Relationship
+	 * */
+	public static LinkedHashSet<LighthouseEvent> getEventsByListEntityAndRel(LighthouseModel model, Collection<LighthouseEntity> listEntity, Collection<LighthouseRelationship> listRel) {
 		LinkedHashSet<LighthouseEvent> listEvents = new LinkedHashSet<LighthouseEvent>();
 		for (LighthouseEntity entity : listEntity) {
 			listEvents.addAll(model.getEvents(entity));
@@ -82,8 +101,10 @@ public class LighthouseModelUtil {
 		return listEvents;
 	}
 
-	public static Collection<LighthouseRelationship> getRelationships(
-			LighthouseModel model, Collection<LighthouseEntity> listEntity) {
+	/**
+	 * Get Relationship related to a list of Entities
+	 * */
+	public static Collection<LighthouseRelationship> getRelationships(LighthouseModel model, Collection<LighthouseEntity> listEntity) {
 		LinkedHashSet<LighthouseRelationship> listRel = new LinkedHashSet<LighthouseRelationship>();
 		for (LighthouseEntity entity : listEntity) {
 			for (LighthouseRelationship rel : model.getRelationshipsFrom(entity)) {
@@ -96,27 +117,6 @@ public class LighthouseModelUtil {
 			}
 		}
 		return listRel;
-	}
-
-	public static Collection<LighthouseEntity> getEntitiesInsideClasses(
-			LighthouseModel model, Collection<String> listClazzFqn) {
-		LinkedHashSet<LighthouseEntity> listEntity = new LinkedHashSet<LighthouseEntity>();
-		for (String clazzFqn : listClazzFqn) {
-			LighthouseEntity clazz = model.getEntity(clazzFqn);
-			listEntity.add(clazz);
-			listEntity.addAll(getEntitiesInsideClass(model, clazz));
-		}
-		return listEntity;
-	}
-			
-	private static LinkedHashSet<LighthouseEntity> getEntitiesInsideClass(LighthouseModel model, LighthouseEntity clazz) {
-		LinkedHashSet<LighthouseEntity> listEntity = new LinkedHashSet<LighthouseEntity>();
-		Collection<LighthouseRelationship> listRelInside = model.getRelationshipsTo(clazz,TYPE.INSIDE);
-		for (LighthouseRelationship rel : listRelInside) {
-			LighthouseEntity entity = rel.getFromEntity();
-			listEntity.add(entity);
-		}
-		return listEntity;
 	}
 	
 }
