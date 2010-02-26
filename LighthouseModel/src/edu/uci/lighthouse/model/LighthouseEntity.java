ba6229a2bc5f37d2110e9add5e5f3ec002943b0d
nmoura <nmoura@bcbb7e22-1d7d-42ea-b575-07c612254b40>
2010-02-26 09:17:26 +0000

    RESOLVED - bug 21: Fix primary key of LIghthouse Entity - from 300 to 500 chars https://calico.ics.uci.edu/bugs/show_bug.cgi?id=21

--- LighthouseModel/src/edu/uci/lighthouse/model/LighthouseEntity.java
+++ LighthouseModel/src/edu/uci/lighthouse/model/LighthouseEntity.java
@@ -1,7 +1,5 @@
 package edu.uci.lighthouse.model;
 
-import java.math.BigInteger;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 import javax.persistence.Column;
