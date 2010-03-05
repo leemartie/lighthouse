baef327f7773812dc0e90d0ea5b039fecd4713c5
tproenca <tproenca@bcbb7e22-1d7d-42ea-b575-07c612254b40>
2010-03-05 23:34:08 +0000

    - Remove subclipse dependencies from the feature and place them in the site.

--- LighthouseSite/deploy.sh
+++ LighthouseSite/deploy.sh
@@ -3,7 +3,7 @@
 RSYNC=/usr/bin/rsync 
 RUSER=lighthouse 
 RHOST=calico.ics.uci.edu 
-RPATH=/home/lighthouse/public_html/updates 
+RPATH=/home/lighthouse/public_html/test 
 LPATH=. 
 
 $RSYNC --exclude=deploy.sh -az $LPATH -e ssh $RUSER@$RHOST:$RPATH 
