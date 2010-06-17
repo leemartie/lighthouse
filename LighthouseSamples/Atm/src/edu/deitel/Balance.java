14ab9d0b77a7b6aca3653a1d5e009e203f14d6e3
nmangano <nmangano@bcbb7e22-1d7d-42ea-b575-07c612254b40>
2010-06-17 17:10:53 +0000

    Committing changes

---
+++ LighthouseSamples/Atm/src/edu/deitel/Balance.java
@@ -0,0 +1,19 @@
+package edu.deitel;
+
+import edu.deitel.transactions.Transaction;
+
+public class Balance extends Transaction {
+
+	public Balance(int userAccountNumber, Screen atmScreen,
+			BankDatabase atmBankDatabase) {
+		super(userAccountNumber, atmScreen, atmBankDatabase);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
