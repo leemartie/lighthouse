985b13205574f0a3c1e9c37cece885698c827ced
nmoura <nmoura@bcbb7e22-1d7d-42ea-b575-07c612254b40>
2010-10-25 22:58:58 +0000

    remove artifacts before create update action

--- LighthouseCore/src/edu/uci/lighthouse/core/dbactions/pull/UpdateAction.java
+++ LighthouseCore/src/edu/uci/lighthouse/core/dbactions/pull/UpdateAction.java
@@ -1,8 +1,6 @@
 package edu.uci.lighthouse.core.dbactions.pull;
 
 import edu.uci.lighthouse.core.controller.WorkingCopy;
-import edu.uci.lighthouse.model.LighthouseModel;
-import edu.uci.lighthouse.model.LighthouseModelManager;
 import edu.uci.lighthouse.model.jpa.JPAException;
 
 public class UpdateAction  extends AbstractWorkingCopyAction {
@@ -15,8 +13,6 @@ public class UpdateAction  extends AbstractWorkingCopyAction {
 
 	@Override
 	public void run() throws JPAException {
-		LighthouseModelManager modelManager = new LighthouseModelManager(LighthouseModel.getInstance());
-		modelManager.removeArtifactsAndEvents(getWorkingCopy().keySet());
 		CheckoutAction checkoutAction = new CheckoutAction(getWorkingCopy());
 		checkoutAction.run();
 	}
