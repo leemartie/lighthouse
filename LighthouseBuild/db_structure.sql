3072378fb7dd4b6e1a301eb86c3f30332e0ca43d
nmoura <nmoura@bcbb7e22-1d7d-42ea-b575-07c612254b40>
2010-11-19 00:06:00 +0000

    ENGINE=MyISAM

--- LighthouseBuild/db_structure.sql
+++ LighthouseBuild/db_structure.sql
@@ -1,72 +1,62 @@
+
 DROP DATABASE lighthouse;
 CREATE DATABASE lighthouse;
+USE lighthouse;
 
-CREATE TABLE lighthouse.LighthouseAuthor (
+# Sequel Pro dump
+# Version 2492
+# http://code.google.com/p/sequel-pro
+#
+# Host: 127.0.0.1 (MySQL 5.1.51)
+# Database: lighthouse
+# Generation Time: 2010-11-18 15:56:22 -0800
+# ************************************************************
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table LighthouseAuthor
+# ------------------------------------------------------------
+
+CREATE TABLE `LighthouseAuthor` (
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-CREATE TABLE lighthouse.LighthouseEntity (
+
+
+# Dump of table LighthouseClass
+# ------------------------------------------------------------
+
+CREATE TABLE `LighthouseClass` (
+  `id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK54B36FAEB691686` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table LighthouseEntity
+# ------------------------------------------------------------
+
+CREATE TABLE `LighthouseEntity` (
   `id` varchar(255) NOT NULL,
   `fullyQualifiedName` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE lighthouse.LighthouseClass (
-  `id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK54B36FAEB691686` (`id`),
-  CONSTRAINT `FK54B36FAEB691686` FOREIGN KEY (`id`) REFERENCES `lighthouseentity` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE lighthouse.LighthouseExternalClass (
-  `id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK248E2CA3B691686` (`id`),
-  CONSTRAINT `FK248E2CA3B691686` FOREIGN KEY (`id`) REFERENCES `lighthouseentity` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE lighthouse.LighthouseField (
-  `id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK54DC6730B691686` (`id`),
-  CONSTRAINT `FK54DC6730B691686` FOREIGN KEY (`id`) REFERENCES `lighthouseentity` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE lighthouse.LighthouseInterface (
-  `id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK7A9286FB691686` (`id`),
-  CONSTRAINT `FK7A9286FB691686` FOREIGN KEY (`id`) REFERENCES `lighthouseentity` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE lighthouse.LighthouseMethod (
-  `id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK5270D16BB691686` (`id`),
-  CONSTRAINT `FK5270D16BB691686` FOREIGN KEY (`id`) REFERENCES `lighthouseentity` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE lighthouse.LighthouseModifier (
-  `id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK6F3D08E1B691686` (`id`),
-  CONSTRAINT `FK6F3D08E1B691686` FOREIGN KEY (`id`) REFERENCES `lighthouseentity` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
-CREATE TABLE lighthouse.LighthouseRelationship (
-  `type` int(11) NOT NULL,
-  `to_id` varchar(255) NOT NULL DEFAULT '',
-  `from_id` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`from_id`,`to_id`,`type`),
-  KEY `FK1AEB674211FF96CA` (`to_id`),
-  KEY `FK1AEB6742E7FC4B3B` (`from_id`),
-  CONSTRAINT `FK1AEB6742E7FC4B3B` FOREIGN KEY (`from_id`) REFERENCES `lighthouseentity` (`id`),
-  CONSTRAINT `FK1AEB674211FF96CA` FOREIGN KEY (`to_id`) REFERENCES `lighthouseentity` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE lighthouse.LighthouseEvent (
+# Dump of table LighthouseEvent
+# ------------------------------------------------------------
+
+CREATE TABLE `LighthouseEvent` (
   `id` varchar(255) NOT NULL,
   `committedTime` datetime DEFAULT NULL,
   `isCommitted` bit(1) NOT NULL,
@@ -80,25 +70,81 @@ CREATE TABLE lighthouse.LighthouseEvent (
   PRIMARY KEY (`id`),
   KEY `FK54D438D0DF44E862` (`entity_id`),
   KEY `FK54D438D03432CE02` (`relationship_from_id`,`relationship_to_id`,`relationship_type`),
-  KEY `FK54D438D02F7797D2` (`author_name`),
-  CONSTRAINT `FK54D438D02F7797D2` FOREIGN KEY (`author_name`) REFERENCES `lighthouseauthor` (`name`),
-  CONSTRAINT `FK54D438D03432CE02` FOREIGN KEY (`relationship_from_id`, `relationship_to_id`, `relationship_type`) REFERENCES `lighthouserelationship` (`from_id`, `to_id`, `type`),
-  CONSTRAINT `FK54D438D0DF44E862` FOREIGN KEY (`entity_id`) REFERENCES `lighthouseentity` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `FK54D438D02F7797D2` (`author_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-CREATE TABLE lighthouse.LighthouseRepositoryEvent (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `className` varchar(255) DEFAULT NULL,
-  `eventTime` datetime DEFAULT NULL,
-  `type` int(11) DEFAULT NULL,
-  `versionAffected` bigint(20) DEFAULT NULL,
-  `author_name` varchar(255) DEFAULT NULL,
+
+DELIMITER ;;
+/*!50003 SET SESSION SQL_MODE="" */;;
+/*!50003 CREATE */ /*!50017 DEFINER=`root`@`localhost` */ /*!50003 TRIGGER `TRIGGER_ADDING_EVENT` BEFORE INSERT ON `LighthouseEvent` FOR EACH ROW BEGIN
+		SET NEW.timestamp=now();
+		IF ( (NEW.isCommitted=TRUE) AND (NEW.timestamp > NEW.committedTime) ) THEN
+			SET NEW.timestamp=NEW.committedTime;
+		END IF;
+	END */;;
+DELIMITER ;
+/*!50003 SET SESSION SQL_MODE=@OLD_SQL_MODE */;
+
+
+# Dump of table LighthouseExternalClass
+# ------------------------------------------------------------
+
+CREATE TABLE `LighthouseExternalClass` (
+  `id` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK6C6020262F7797D2` (`author_name`),
-  CONSTRAINT `FK6C6020262F7797D2` FOREIGN KEY (`author_name`) REFERENCES `lighthouseauthor` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `FK248E2CA3B691686` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-CREATE TABLE lighthouse.LighthouseQuestion (
+
+
+# Dump of table LighthouseField
+# ------------------------------------------------------------
+
+CREATE TABLE `LighthouseField` (
+  `id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK54DC6730B691686` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table LighthouseInterface
+# ------------------------------------------------------------
+
+CREATE TABLE `LighthouseInterface` (
+  `id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK7A9286FB691686` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table LighthouseMethod
+# ------------------------------------------------------------
+
+CREATE TABLE `LighthouseMethod` (
+  `id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK5270D16BB691686` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table LighthouseModifier
+# ------------------------------------------------------------
+
+CREATE TABLE `LighthouseModifier` (
+  `id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK6F3D08E1B691686` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table LighthouseQuestion
+# ------------------------------------------------------------
+
+CREATE TABLE `LighthouseQuestion` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `subject` varchar(255) DEFAULT NULL,
   `text` varchar(255) DEFAULT NULL,
@@ -107,22 +153,50 @@ CREATE TABLE lighthouse.LighthouseQuestion (
   `lhClass_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK4E6F41F0DFAA39AE` (`lhClass_id`),
-  KEY `FK4E6F41F02F7797D2` (`author_name`),
-  CONSTRAINT `FK4E6F41F02F7797D2` FOREIGN KEY (`author_name`) REFERENCES `lighthouseauthor` (`name`),
-  CONSTRAINT `FK4E6F41F0DFAA39AE` FOREIGN KEY (`lhClass_id`) REFERENCES `lighthouseclass` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `FK4E6F41F02F7797D2` (`author_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-DELIMITER |
-CREATE TRIGGER lighthouse.TRIGGER_ADDING_EVENT BEFORE INSERT ON LighthouseEvent
-	FOR EACH ROW
-	BEGIN
-		SET NEW.timestamp=now();
-		IF ( (NEW.isCommitted=TRUE) AND (NEW.timestamp > NEW.committedTime) ) THEN
-			SET NEW.timestamp=NEW.committedTime;
-		END IF;
-	END
-|
-DELIMITER ;
+
+
+# Dump of table LighthouseRelationship
+# ------------------------------------------------------------
+
+CREATE TABLE `LighthouseRelationship` (
+  `type` int(11) NOT NULL,
+  `to_id` varchar(255) NOT NULL DEFAULT '',
+  `from_id` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`from_id`,`to_id`,`type`),
+  KEY `FK1AEB674211FF96CA` (`to_id`),
+  KEY `FK1AEB6742E7FC4B3B` (`from_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table LighthouseRepositoryEvent
+# ------------------------------------------------------------
+
+CREATE TABLE `LighthouseRepositoryEvent` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `className` varchar(255) DEFAULT NULL,
+  `eventTime` datetime DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `versionAffected` bigint(20) DEFAULT NULL,
+  `author_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK6C6020262F7797D2` (`author_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 DROP USER 'lighthouse'@'%';
 DROP USER 'lighthouse'@'localhost';
