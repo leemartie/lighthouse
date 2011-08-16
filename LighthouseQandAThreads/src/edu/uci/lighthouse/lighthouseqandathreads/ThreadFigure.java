6990d46da3456998d05beb39568627a26f45598d
lmartie <lmartie@bcbb7e22-1d7d-42ea-b575-07c612254b40>
2011-08-16 08:33:00 +0000

    added question button

--- LighthouseQandAThreads/src/edu/uci/lighthouse/lighthouseqandathreads/ThreadFigure.java
+++ LighthouseQandAThreads/src/edu/uci/lighthouse/lighthouseqandathreads/ThreadFigure.java
@@ -3,21 +3,29 @@ package edu.uci.lighthouse.lighthouseqandathreads;
 import edu.uci.lighthouse.ui.figures.CompartmentFigure;
 import edu.uci.lighthouse.ui.figures.ILighthouseClassFigure.MODE;
 import edu.uci.lighthouse.ui.figures.CompartmentFigure;
+
+import org.eclipse.draw2d.FlowLayout;
+import org.eclipse.draw2d.GridLayout;
 import org.eclipse.draw2d.Panel;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.draw2d.Button;
+import org.eclipse.draw2d.geometry.Rectangle;
 
 public class ThreadFigure extends CompartmentFigure {
 	Button questionButton;
+	private GridLayout layout;
 
 	public ThreadFigure() {
+		layout = new GridLayout();
+		layout.numColumns = 2;
+		setLayoutManager(layout);
 		Image icon = 
 			AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, "/icons/question.png").createImage();
 		questionButton =  new org.eclipse.draw2d.Button(icon);
-		this.add(questionButton);
+		
 
 	}
 
@@ -29,7 +37,7 @@ public class ThreadFigure extends CompartmentFigure {
 
 	@Override
 	public void populate(MODE mode) {
-		
+		this.add(questionButton, new Rectangle(0, 0, 10, 10));
 	}
 
 }
